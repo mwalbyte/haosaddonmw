e6d69a49f858781ac5f764b8e89cf4fb94ac7293
mwalbyte <156855795+mwalbyte@users.noreply.github.com>
2025-09-20 00:06:09 +0200

    Update run.sh

--- homepage/run.sh
+++ homepage/run.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
-# Zorg dat /config bestaat en link naar /app/config
 mkdir -p /config
-mkdir -p /app
+mkdir -p /app/config
 rm -rf /app/config
 ln -s /config /app/config
 
-# Start Homepage
-exec /docker-entrypoint.sh
+exec /usr/local/bin/docker-entrypoint.sh
